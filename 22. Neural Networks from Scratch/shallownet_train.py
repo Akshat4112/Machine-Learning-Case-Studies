8ea7e360bb9a817b3816275a1fd1958eab114e1a
Akshat Gupta <akshat41121995@gmail.com>
2020-05-12 15:22:31 +0530

    vggnet from scratch added

---
+++ 22. Neural Networks from Scratch/shallownet_train.py
@@ -0,0 +1,59 @@
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from preprocessing.imagetoarraypreprocessor import ImageToArrayPreprocessor
+from preprocessing.simplepreprocessor import SimplePreprocessor
+from datasets.simpledatasetloader import SimpleDatasetLoader
+from ShallowNet.nn.conv.shallownet import ShallowNet
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+help="path to output model")
+args = vars(ap.parse_args())
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the image preprocessors
+sp = SimplePreprocessor(32, 32)
+iap = ImageToArrayPreprocessor()
+
+# load the dataset from disk then scale the raw pixel intensities
+# to the range [0, 1]
+sdl = SimpleDatasetLoader(preprocessors=[sp, iap])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.astype("float") / 255.0
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+test_size=0.25, random_state=42)
+
+trainY = LabelBinarizer().fit_transform(trainY)
+testY = LabelBinarizer().fit_transform(testY)
+
+print("[INFO] compiling model...")
+opt = SGD(lr=0.005)
+model = ShallowNet.build(width=32, height=32, depth=3, classes=3)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network...")
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+batch_size=32, epochs=100, verbose=1)
+
+print("[INFO] serializing network . . .")
+model.save(args["model"])
+
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+predictions.argmax(axis=1),
+target_names=["cat", "dog", "panda"]))
+
